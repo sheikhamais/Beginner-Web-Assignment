6a49959f1d318f300ba3b1913518fb3c85812452
sheikhamais <sheikhamais@gmail.com>
2025-01-16 16:20:48 +0500

    contact us header and styling structure setup

--- Assignment/main.js
+++ Assignment/main.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('scroll-to-top');
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 600) {
+    if (window.scrollY > 200) {
         button.classList.add('visible-scroll-to-top-button');
     } else {
         button.classList.remove('visible-scroll-to-top-button');
