3fc947a97e3679009c1777140b9c88eca46b273d
sheikhamais <sheikhamais@gmail.com>
2025-01-20 17:08:08 +0500

    makes nav bar responsive

--- Assignment/main.js
+++ Assignment/main.js
@@ -66,3 +66,14 @@ function showErrorAlert(message) {
         confirmButtonText: "Cool"
     });
 }
+
+const navItems = document.getElementById('nav-items');
+let navToggleMenuButton = document.getElementById('hamburger-menu-button-anchor');
+navToggleMenuButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (navItems.style.display === 'none' || navItems.style.display === '') {
+        navItems.style.display = 'block';
+    } else {
+        navItems.style.display = 'none';
+    }
+})
